606dc9283232ba17c4ea5b655e84bd2a895470b9
Alena <alena.karpov@gmail.com>
2023-05-29 11:37:15 +0100

    style: add tabs to portal

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -7,6 +7,26 @@ window.addEventListener('DOMContentLoaded', () => {
           ok = document.querySelector('#ok'),
           loginForm = document.querySelector('.login-form'),
           forEmployeesLink = document.querySelector('.menu__link_empl');
+		  loginSumbitButton = document.getElementById('loginSubmit');	
+	      agreed = localStorage.getItem('policyAgreed');	
+
+	
+	
+    	const logField = () => {
+		return document.querySelector('input[name="login"]');
+	}	
+    	const passField = () => {
+		return document.querySelector('input[name="password"]');
+	}	
+	const employees = () => {
+		return document.querySelector('.employees');
+	}
+     	
+	
+
+    if (agreed !== null && agreed !== undefined && agreed === 'yes') {
+	cookie.classList.remove('cookie_active');
+    }
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
@@ -22,16 +42,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
     ok.addEventListener('click', () => {
         cookie.classList.remove('cookie_active');
+	localStorage.setItem('policyAgreed', 'yes');
     });
 
     forEmployeesLink.addEventListener('click', () => {
         loginForm.classList.add('login-form_active');
     });
 
-    const login = () => {
-        const login = document.querySelector('input[name="login"]').value;
-        const pass = document.querySelector('input[name="password"]').value;
-    
+    loginSumbitButton.addEventListener('click', () => {
+        if (document.querySelector('#loginSubmit>span').innerHTML.toLowerCase() === 'sign in') {
+
+        let login = logField().value;
+        let pass = passField().value;
+	
+	if (login !== null && login !== undefined && login.length > 2) {
         const url = 'https://test-shmest.com/back/signin'
     
     
@@ -40,12 +64,51 @@ window.addEventListener('DOMContentLoaded', () => {
            headers: { 'Content-Type': 'application/json' },
            body: JSON.stringify({ email: login, password: pass})
           }).then(response => response.json())
-          .then(data => {
-              let tkn = data.token
-              localStorage.setItem('token', tkn)
-              loginForm.classList.remove('login-form_active');
+          .then(data => {     
+     let tkn = data.token
+		if (tkn !== null && tkn !== undefined && tkn.length > 10) {
+		debugger
+             	localStorage.setItem('token', tkn)
+                handleLogin();
+		// let style = 'display: none;';
+		// loginField.setAttribute('style', style);
+		// passWordField.setAttribute('style', style);
+           //  loginForm.classList.remove('login-form_active')
+		}
           })
-    }
+
+	} 
+
+	} else {
+
+		handleLogout();
+	}
+    });
+
+const handleLogout = () => {
+		localStorage.removeItem('token')
+    		let style = 'display: block;';
+		let hideStyle = 'display: none;';
+		logField().setAttribute('style', style);
+		passField().setAttribute('style', style);
+		document.querySelector('#loginSubmit>span').innerHTML = 'Sign in' 
+		employees().setAttribute('style', hideStyle);
+}
+
+
+
+	const handleLogin = () => {
+    		let style = 'display: none;';
+		let displayStyle = 'display: block;'
+		logField().setAttribute('style', style);
+		passField().setAttribute('style', style);
+		document.querySelector('#loginSubmit>span').innerHTML = 'Sign out'
+		employees().setAttribute('style', displayStyle); 
+		employees().scrollIntoView();
+		
+	}
+
+
 
 
     if (window.innerWidth < 768) {
@@ -59,5 +122,30 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+	const getEnabledIds = () => {
+
+	return new Promise((resolve) => {
+  	var token = localStorage.getItem('token');
+	var url = 'https://test-shmest.com/back/amds_enabled_sheets'
+
+	fetch(url, {
+     	method: 'GET',
+      	headers: { 'Content-Type': 'application/json', 
+                 'token': token}
+	}).then(resp => resp.json())
+  	.then(response => {
+        resolve(response.message)
+  	})    
+	} )  
+
+	}
+
+getEnabledIds().then(ids => console.log(ids))
+
+
+handleLogout();
     
 });
+
+
+
